0e80c81b2f39c0d9a6aa0dbfedf241999580fda6
logunov28 <79602109+logunov28@users.noreply.github.com>
2022-11-14 19:35:11 +0300

    v2
    Now works with every version of PyQt.

--- ui_to_py.py
+++ ui_to_py.py
@@ -1,9 +1,24 @@
 import os
 
-print("""Это конвертер для преобразования файла, созданного в QtCreator, в файл с расширением py.
-Пока работает только с pyqt6.""")
-ui_file = input('Введите путь к исходному файлу:')
-py_file = input('Введите имя, под которым нужно сохранить файл (например, gui.py):')
-os.popen('pyuic6 ' + ui_file+ ' -o ' + py_file)
-print('Готово! Результат лежит в одной папке с этим скриптом. Нажмите любую клавишу, чтобы выйти.')
-os.popen('pause')
+# Узнаём версию PyQt
+pip_list = os.popen('pip list').read().split('\n')
+qt_version = None
+for i in range(len(pip_list)):
+    if 'PyQt' in pip_list[i]:
+        qt = pip_list[i][0:5]
+        qt_version = qt[-1]
+        break
+
+
+if qt_version is None:
+    print('Библиотека PyQt не установлена!')
+    os.popen('pause')
+
+# Конвертируем
+else:
+    print("Это конвертер для преобразования файла, созданного в QtCreator, в файл с расширением py.")
+    ui_file = input('Введите путь к исходному файлу:')
+    py_file = input('Введите имя, под которым нужно сохранить файл (например, gui.py):')
+    os.popen('pyuic' + qt_version + ' ' + ui_file+ ' -o ' + py_file)
+    print('Готово! Результат лежит в одной папке с этим скриптом. Нажмите любую клавишу, чтобы выйти.')
+    os.popen('pause')
